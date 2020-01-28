4f9ccd69520bc4b584d2ff638231087dfc6fe2f2
Gabriel Rodrigues <jipacoding@gmail.com>
2020-01-28 10:01:33 -0400

    poc001a: funcionamento basico

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,261 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="border-1 game">
+      <div class="border-2 game-header">
+        <div>
+          <span class="game-header-counter">
+            {{ game.options.bombsCount.toString().padStart(3, '0') }}
+          </span>
+        </div>
+        <div>
+          <div class="game-header-emoji emoji-brbo"></div>
+        </div>
+        <div>
+          <span class="game-header-counter">000</span>
+        </div>
+      </div>
+      <div class="border-2 game-campo">
+        <ul class="game-campo-minas">
+          <li
+            v-for="(_, i) in (game.options.size.rows * game.options.size.columns)"
+            :key="i"
+            class="game-campo-minas-mina"
+            @click="() => { bombClick(i) }"
+          >
+            <!-- só por enquanto -->
+            <div
+              class="mina-overlay"
+              :class="{ closed: !game.discovered.includes(i), bomb: game.bombs.includes(i) }">
+            </div>
+
+            <div class="bombs-around">
+              {{
+                getBombsArround(getTablePosition(i)) > 0
+                  ? getBombsArround(getTablePosition(i))
+                  : ''
+              }}
+              <!-- {{Math.round(Math.random() * 6)}} -->
+            </div>
+            <!-- {{i}} -->
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      game: {
+        options: {
+          bombsCount: 5,
+          size: {
+            rows: 6,
+            columns: 6,
+          },
+        },
+        bombs: [1, 8, 23, 30, 33],
+        discovered: [],
+      },
+    };
+  },
+  methods: {
+    getTablePosition(i) {
+      return {
+        row: Math.floor(i / this.game.options.size.rows),
+        column: i % this.game.options.size.columns,
+      };
+    },
+    getIndexPosition({ row, column }) {
+      return row * this.game.options.size.rows + column;
+    },
+    getSquaresArround(pos, directContact = true) {
+      const firstColumn = pos.column !== 0;
+      const lastColumn = pos.column !== this.game.options.size.columns - 1;
+
+      return [
+        [pos.row - 1, pos.column - 1, directContact && firstColumn],
+        [pos.row - 1, pos.column],
+        [pos.row - 1, pos.column + 1, directContact && lastColumn],
+
+        [pos.row, pos.column - 1, firstColumn],
+        [pos.row, pos.column + 1, lastColumn],
+
+        [pos.row + 1, pos.column - 1, directContact && firstColumn],
+        [pos.row + 1, pos.column],
+        [pos.row + 1, pos.column + 1, directContact && lastColumn],
+      ]
+        .filter(([,, condition = true]) => condition)
+        .map(([row, column]) => this.getIndexPosition({ row, column }));
+    },
+    getBombsArround(pos) {
+      return this.getSquaresArround(pos)
+        .filter(this.hasBomb)
+        .length;
+    },
+    getSafeSqrsArround(pos) {
+      return this.getSquaresArround(pos, true)
+        .filter(i => !this.hasBomb(i))
+        .filter(i => this.getBombsArround(this.getTablePosition(i)) === 0);
+    },
+    hasBomb(i) {
+      if (i >= 0 && i < this.game.options.size.rows * this.game.options.size.columns) {
+        return this.game.bombs.includes(i);
+      }
+      return false;
+    },
+    bombClick(i) {
+      this.discover(i);
+    },
+    discover(i) {
+      if (!this.game.discovered.includes(i)) {
+        this.game.discovered.push(i);
+      }
+    },
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import "~@/assets/scss/app";
+
+$border-width: 4px;
+$border-light: $border-width solid rgba(#ededfc, 0.4);
+$border-shadow: $border-width solid rgba(#030220, 0.4);
+
+.border-1 {
+  border-top: $border-light;
+  border-left: $border-light;
+
+  border-bottom: $border-shadow;
+  border-right: $border-shadow;
+}
+
+.border-2 {
+  border-top: $border-shadow;
+  border-left: $border-shadow;
+
+  border-bottom: $border-light;
+  border-right: $border-light;
+}
+
+#app,
+.game {
+  height: 100%;
+  min-height: 100vh;
+}
+
+.game {
+  width: 100%;
+  height: 100%;
+
+  background-color: #c4c4c4;
+  padding: 4px;
+
+  display: flex;
+  flex-direction: column;
+
+  .game-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    padding: 6px;
+
+    &-counter {
+      display: block;
+
+      background-color: #3b0908;
+      border-radius: 4px;
+
+      padding: 6px;
+
+      font-size: 18px;
+      letter-spacing: 0.125em;
+      color: #fa2622;
+    }
+
+    &-emoji {
+      width: 32px;
+      height: 32px;
+
+      $emojis: "smile", "surprise", "xx", "brbo";
+
+      @each $emoji in $emojis {
+        &.emoji-#{$emoji} {
+          background: url('~@/assets/icons/emoji-#{$emoji}.png');
+          background-size: 100%;
+        }
+      }
+    }
+  }
+
+  .game-campo {
+    flex: 1;
+    padding: 4px;
+
+    display: flex;
+    flex-direction: column;
+
+    &-minas {
+      flex: 1;
+
+      display: grid;
+      grid-template-columns: repeat(6, 1fr);
+      height: 100%;
+
+      &-mina {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: relative;
+        width: 100%;
+
+        background-color: white;
+        .mina-overlay {
+          position: absolute;
+          top: 0;
+          left: 0;
+
+          width: 100%;
+          height: 100%;
+
+          // background-color: red;
+          background: rgba(8, 18, 19, 0.31);
+          // border: $border-width solid rgba(11, 7, 52, 0.1);
+
+          &.bomb {
+            background: url("~@/assets/icons/emoji-bomb.png")
+              rgba(8, 18, 19, 0.31) center center no-repeat;
+
+            background-size: 75%;
+          }
+          &.closed {
+            border-top: $border-light;
+            border-left: $border-light;
+
+            border-bottom: $border-shadow;
+            border-right: $border-shadow;
+
+            background: linear-gradient(
+                0deg,
+                rgba(8, 18, 19, 0.31),
+                rgba(8, 18, 19, 0.31)
+              ),
+              #ffffff;
+            border-radius: 2px;
+            overflow: hidden;
+          }
+        }
+
+        .bombs-around {
+          font-size: 1rem;
+          font-weight: 600;
+        }
+      }
+    }
+  }
 }
 </style>
